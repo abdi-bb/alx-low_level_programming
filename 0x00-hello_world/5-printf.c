e3519efc23a776b8941b216a1fccce9b43ffc92d
Abdi Berhe <abdi201380@gmail.com>
2022-12-08 04:09:00 -0800

    Program written using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,10 +6,10 @@
 #include <stdio.h>
 
 /**
-         * main - prints "with proper grammar, but the outcome is
-	 * 		a piece of art," followed by a new line.
-         * Return: Always 0.
-         */
+ * main - prints "with proper grammar, but the outcome is
+ * a piece of art," followed by a new line.
+ * Return: Always 0.
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
