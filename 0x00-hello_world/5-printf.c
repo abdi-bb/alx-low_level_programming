935337ac9214c40b70581634bfea7104ca242557
Abdi Berhe <abdi201380@gmail.com>
2022-12-08 03:03:22 -0800

    Writing using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+/*
+ * File: 5-printf.c
+ * Auth: Abdi G. Berhe
+ */
+
+#include<stio.h>
+
+/**
+         * main - prints "with proper grammar, but the outcome is a piece of art
+,"
+         * Followed by new line
+         * Return: 0
+         */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		
+	
+	return(0);
+}
