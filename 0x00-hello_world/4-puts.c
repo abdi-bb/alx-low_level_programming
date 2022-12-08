a94ef702d8ba83250b2b007b22011a86475af8f8
Abdi Berhe <abdi201380@gmail.com>
2022-12-08 02:35:00 -0800

    Program written using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/*
+ * File: 4-puts.c
+ * Auth: Abdi G. Berhe
+ */
+
+#include<stdio.h>
+
+/**
+ * Main - prints "Programming is like building a multilingual puzzle"
+ * Followed by new line
+ * Return: always 0
+ */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
