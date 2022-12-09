343e68b1d29975f41264915f750762410391f7ab
Abdi Berhe <abdi201380@gmail.com>
2022-12-09 10:28:35 -0800

    Combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,13 +3,11 @@
  * Auth: Abdi G. Berhe
  */
 
-
 #include <stdio.h>
-
 
 /**
  *  main - Prints all possible combinations of three different digits,
- *  	in ascending order, separated by a comma followed by a space.
+ * 	in ascending order, separated by a comma followed by a space.
  *
  *  Return: Always 0.
  */
@@ -22,6 +20,7 @@ int main(void)
 		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
 			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			{
 				 putchar((digit1 % 10) + '0');
 				 putchar((digit2 % 10) + '0');
 				 putchar((digit3 % 10) + '0');
