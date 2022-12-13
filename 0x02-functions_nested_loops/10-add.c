5a63e2653d60b32ca3692056f365247e6aeddaff
Abdi Berhe <abdi201380@gmail.com>
2022-12-13 13:55:01 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+/*
+ * File: 10-add.c
+ * Auth: Abdi G. Berhe
+ */
+
+#include "main.h"
+
+/**
+ * add - main function
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of both integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
