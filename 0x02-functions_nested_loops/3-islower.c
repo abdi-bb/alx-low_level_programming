b8b92b9b98757c4d007716d13846f440cfe1e2c0
Abdi Berhe <abdi201380@gmail.com>
2022-12-13 12:55:27 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+/*
+ * File: 3-islower.c
+ * Auth: Abdi G. Berhe
+ */
+
+#include "main.h"
+
+/**
+ * _islower - main function
+ *
+ * @c: character to compare
+ *
+ * Return: 1 if true, 0 if false
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
