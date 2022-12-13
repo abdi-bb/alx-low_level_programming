878a5cb21a08b4bc5eb3bb1b4a2124a6ca6d16f5
Abdi Berhe <abdi201380@gmail.com>
2022-12-13 13:48:12 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+/*
+ * File: 9-times_table.c
+ * Auth: Abdi G. Berhe
+ */
+
+#include "main.h"
+
+/**
+ * times_table - times table function
+ *
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int prod = j * i;
+
+			if (j == 0)
+			{
+				_putchar('0');
+			} else if (prod <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + '0');
+			} else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(prod / 10 + '0');
+				_putchar(prod % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
