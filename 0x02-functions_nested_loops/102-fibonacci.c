50ddd1df9e743834019cf7b76fc5a24db5e9c8b8
Abdi Berhe <abdi201380@gmail.com>
2022-12-13 11:51:50 -0800

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+/*
+ * File: 102-fibonacci.c
+ * Auth: Abdi G. Berhe
+ */
+
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Nothing
+ */
+int main(void)
+{
+	int counter = 2;
+	long int a = 1;
+	long int b = a + 1;
+	long int c = a + b;
+
+	printf("%ld, %ld, ", a, b);
+	while (counter < 50)
+	{
+		printf("%ld", c);
+		counter++;
+		a = b;
+		b = c;
+		c = a + b;
+		if (counter < 50)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
