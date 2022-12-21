1f36b40500e8a6eef76ee57eb52760ac7c8acf5d
Abdi Berhe <abdi201380@gmail.com>
2022-12-21 07:32:04 -0800

    Function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,9 @@
 #include "main.h"
 
 /**
- * strcat - Concatenates two strings
+ * strcat - Concatenates the string pointed to by @src, including the
+ * terminating null byte, to the end of the string
+ * pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
  *
