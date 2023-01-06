693fe5add42642494de4d61e5d418ed2cb81803f
Abdi Berhe <abdi201380@gmail.com>
2023-01-06 09:55:52 -0800

    Program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[])
+int main(int __attribute__((__unused__)) argc, char* argv[])
 {
-	if(argc > 1)
-		printf("%d\n", argc);
 	printf("%s\n", argv[0]);
 
 	return (0);
