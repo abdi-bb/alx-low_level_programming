0da5bc4183f312d597dc0f02b4f145f812849f5a
Abdi Berhe <abdi201380@gmail.com>
2023-01-06 10:01:13 -0800

    Program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,17 @@
+/*
+ * File: 0-whatsmyname.c
+ * Auth: Abdi G. Berhe
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - prints its name
+ * @argv[]: argument vector
+ *
+ * Return: returns 0 for success
+ */
 int main(int __attribute__((__unused__)) argc, char* argv[])
 {
 	printf("%s\n", argv[0]);
