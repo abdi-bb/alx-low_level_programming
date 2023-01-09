e503a7428b23e0dcf736734cbd5a88bd4105ade9
Abdi Berhe <abdi201380@gmail.com>
2023-01-09 12:11:07 -0800

    Task 0 and 1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+ * create_array - Creates an array of chars and intializes with a specific char.
+ * @size: Size of the array to be created.
+ * @c: A specific character for intialization.
+ *
+ * Return: Returns a pointer to the array or returns NULL for 0 size or for failur
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	
+	array = (char *)malloc(size * sizeof(c));
+
+	if(array == NULL)
+		return (NULL);
+
+	for(i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+}
+
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup - function that returns a pointer to newly allocated space in memory.
+ * @str: The string given as a parameter.
+ *
+ * Return: NULL if str = NULL or for insufficient memory, returns a pointer to 
+ * 	the duplicated string on success.
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int len = 0, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	for(i = 0; str[i]; i++)
+	{
+		len++;
+	}
+
+	dup = malloc((len + 1) * sizeof(char));
+
+	if (dup == NULL)
+		return (NULL);
+
+	for(i = 0; str[i]; i++)
+		dup[i] = str[i];
+
+	dup[len] = '\0';
+
+	return (dup);
+}
