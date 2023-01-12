042846d6fde4714322f266fcd07344ee0eb6101e
Abdi Berhe <abdi201380@gmail.com>
2023-01-12 04:31:33 -0800

    String concatenation function

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s3;
-	unsigned int i, is3 = 0, len = 0;
+	unsigned int i, is3 = 0, len = n;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -21,7 +21,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; i < s1[i] + n; i++)
+	for (i = 0; s1[i]; i++)
 		len++;
 
 	s3 = malloc(sizeof(*s3) * len + 1);
@@ -32,10 +32,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i]; i++)
 		s3[is3++] = s1[i];
 
-	for (i = 0; i < n; i++)
+	for (i = 0; s2[i] && i < n; i++)
 		s3[is3++] = s2[i];
 
-	s3[is3] = '\0';
+	s3[len] = '\0';
 
 	return (s3);
 }
