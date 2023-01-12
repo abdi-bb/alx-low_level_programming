c5152c8e02ec61495c0eda785cc891b688cf56fa
Abdi Berhe <abdi201380@gmail.com>
2023-01-12 09:10:58 -0800

    Array of intigers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,17 +11,19 @@
  */
 int *array_range(int min, int max)
 {
-	int *r, i;
+	int *r, i, size;
 
 	if (min > max)
 		return (NULL);
 
-	r = malloc(sizeof(int) * (max - min + 1));
+	size = max - min + 1;
+
+	r = malloc(sizeof(int) * size);
 
 	if (r == NULL)
 		return (NULL);
 
-	for (i = 0; i < (max - min + 1); i++)
+	for (i = 0; i < size; i++)
 			r[i] = min++;
 
 
