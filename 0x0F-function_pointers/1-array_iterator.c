cbf32d5a9da49149e641d9a5f38bbaa7c4b260b9
Abdi Berhe <abdi201380@gmail.com>
2023-01-18 11:47:19 -0800

    Function that executes another function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	int i;
 
 	if (array == NULL || action == NULL)
-		return (NULL):
+		return (NULL);
 
 	for (i = 0; i < (int) size; i++)
 	{
