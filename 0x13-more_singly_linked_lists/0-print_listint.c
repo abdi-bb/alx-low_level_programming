44ebe646248e501e1fbaf5e97394c8283edb9289
Abdi Berhe <abdi201380@gmail.com>
2023-01-31 08:41:59 +0300

    Adding function definition

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
-
+/**
+ * print_listint - Prints all the elements of listint_t
+ * @h : Node to be passed as an argument
+ *
+ * Return: The number of nodes.
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
