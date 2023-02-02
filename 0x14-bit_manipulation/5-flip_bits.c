fc0111af8a01f3f0eec0c24600437d6dc3482d09
Abdi Berhe <abdi201380@gmail.com>
2023-02-02 23:51:22 +0300

    Function that returns number of bits to be flipped

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,15 +3,15 @@
 
 /**
  * bit_flips_needed - Returns the number of bits to be flipped
- * @a: The first number
- * @b: The second number we will get
+ * @n: The first number
+ * @m: The second number we will get
  *
  * Return: Number of bits to be flipped
  */
-unsigned int bit_flips_needed(unsigned long int a, unsigned long int b)
+unsigned int bit_flips_needed(unsigned long int n, unsigned long int m)
 {
 	unsigned int count = 0;
-	unsigned long int c = a ^ b;
+	unsigned long int c = n ^ m;
 
 	while (c > 0)
 	{
