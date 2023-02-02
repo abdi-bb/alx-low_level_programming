28642dce63ba24a5c0b237b7ab57bd7e214eede6
Abdi Berhe <abdi201380@gmail.com>
2023-02-02 21:59:18 +0300

    Function that changes binary to integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * binary_to_unit - Converts binary to unsigned int
+ * binary_to_uint - Converts binary to unsigned int
  * @b: A pionter to a string of 0 and 1 chars
  *
  * Return: Unsigned int, or 0 if
@@ -11,19 +11,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int num = 0;
-    int i;
-
-    if (b == NULL)
-        return 0;
-
-    for (i = 0; b[i]; i++)
-    {
-        if (b[i] != '0' && b[i] != '1')
-            return 0;
-
-        num = (num << 1) + (b[i] - '0');
-    }
-    return num;
+	unsigned int num = 0;
+	int i;
+	
+	if (b == NULL)
+		return (0);
+	
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return 0;
+		
+		num = (num << 1) + (b[i] - '0');
+	}
+	return num;
 }
 
