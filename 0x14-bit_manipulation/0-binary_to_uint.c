b3dc7b8eda3d33bb22719cfc4e3353c9f7b009e4
Abdi Berhe <abdi201380@gmail.com>
2023-02-02 22:03:34 +0300

    Function that changes binary to integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,17 +13,17 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
 	int i;
-	
+
 	if (b == NULL)
 		return (0);
-	
+
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
-			return 0;
-		
+			return (0);
+
 		num = (num << 1) + (b[i] - '0');
 	}
-	return num;
+	return (num);
 }
 
