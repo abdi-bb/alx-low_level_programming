07f41a11375ef77707ac95c8f77cd398e3dbd4ab
Abdi Berhe <abdi201380@gmail.com>
2023-02-02 22:07:59 +0300

    Function that prints binary of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,8 +9,9 @@
  */
 void print_binary(unsigned long int n)
 {
-    if (n > 1)
-        print_binary(n >> 1);
-    write(1, ((n & 1) == 0) ? "0" : "1", 1);
+	if (n > 1)
+		print_binary(n >> 1);
+
+	write(1, ((n & 1) == 0) ? "0" : "1", 1);
 }
 
