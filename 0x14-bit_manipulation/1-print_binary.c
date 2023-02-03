153e94a779e42d6f1f286e6f4dec47eda4d8635a
Abdi Berhe <abdi201380@gmail.com>
2023-02-03 18:57:22 +0300

    Updating to use _putchar

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,10 +10,9 @@ void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);
-
+		
 	if ((n & 1) == 0)
-        _putchar('0');
-    else
-        _putchar('1');
+		_putchar('0');
+	else
+		_putchar('1');
 }
-
