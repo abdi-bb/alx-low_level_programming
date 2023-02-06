d860544f6f2dc98af6d72ee5ba2b3f9ebf0141cb
Abdi Berhe <abdi201380@gmail.com>
2023-02-06 20:32:06 +0300

    Creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,29 +9,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	if (filename == NULL)
-	{
-		return (-1);
-	}
-	FILE *fp = fopen(filename, "w");
+	int o, w, len = 0;
 
-	if (fp == NULL)
-	{
+	if (filename == NULL)
 		return (-1);
-	}
+
 	if (text_content != NULL)
 	{
-		int result = fputs(text_content, fp);
-
-		if (result < 0)
-		{
-			fclose(fp);
-			return (-1);
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	fclose(fp);
-	int success = chmod(filename, S_IRUSR | S_IWUSR);
 
-	return ((success == 0) ? 1 : -1);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
 
