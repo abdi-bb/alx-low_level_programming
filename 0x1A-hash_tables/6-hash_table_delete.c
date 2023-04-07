3df9016c3708cc6eea108a390ee1fa033fbc6910
Abdi Berhe <abdi201380@gmail.com>
2023-04-07 15:09:12 -0700

    Deletes hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table to be deleted
+ *
+ * Return: Nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+    unsigned long int i;
+    hash_node_t *node, *tmp;
+
+    if (!ht)
+        return;
+
+    for (i = 0; i < ht->size; i++)
+    {
+        node = ht->array[i];
+        while (node)
+        {
+            tmp = node;
+            node = node->next;
+            free(tmp->key);
+            free(tmp->value);
+            free(tmp);
+        }
+    }
+
+    free(ht->array);
+    free(ht);
+}
+
