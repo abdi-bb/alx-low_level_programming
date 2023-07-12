801f73606b46d2d9fc9dfefea3b83ba7183a8cca
Abdi Berhe <abdi201380@gmail.com>
2023-07-12 05:05:59 -0700

    [Update]: Add prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,7 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+void print_array(int *array, size_t start, size_t end);
 
 #endif /* SEARCH_ALGOS */
